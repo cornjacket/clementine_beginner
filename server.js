74e468a74025f61af306397d7d6f2cf54a2d0bd2
cornjacket <taylor.david.ray@gmail.com>
2015-11-16 21:19:08 +0000

    server.js cleanup

--- server.js
+++ server.js
@@ -2,25 +2,28 @@
 
 var express = require('express');
 var routes = require('./app/routes/index.js');
-var mongo = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
 
 var app = express();
 
-mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
+
+mongoose.connect('mongodb://localhost:27017/clementinejs');
+/*mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
 
     if (err) {
         throw new Error('Database failed to connect!');
     } else {
         console.log('MongoDB successfully connected on port 27017.');
     }
-
+*/
     app.use('/public', express.static(process.cwd() + '/public'));
     app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-    routes(app, db);
+    routes(app) //, db);
 
-    app.listen(8080, function () {
-        console.log('Listening on port 8080...');
+    var port = 8080
+    app.listen(port, function () {
+        console.log('Listening on port '+port+'...');
     });
 
-});
+//});
