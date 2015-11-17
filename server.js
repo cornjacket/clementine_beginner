43d0c208c15491a0dfe5e1def3b5cb32988e6d19
cornjacket <taylor.david.ray@gmail.com>
2015-11-17 19:34:16 +0000

    update the server file

--- server.js
+++ server.js
@@ -1,29 +1,33 @@
 'use strict';
 
-var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
+var express = require('express'),
+    routes = require('./app/routes/index.js'),
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    session = require('express-session');
 
 var app = express();
+require('dotenv').load();
+require('./app/config/passport')(passport);
 
+mongoose.connect(process.env.MONGO_URI);
 
-mongoose.connect('mongodb://localhost:27017/clementinejs');
-/*mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-    if (err) {
-        throw new Error('Database failed to connect!');
-    } else {
-        console.log('MongoDB successfully connected on port 27017.');
-    }
-*/
-    app.use('/public', express.static(process.cwd() + '/public'));
-    app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+app.use(session({
+    secret: 'secretClementine',
+    resave: false,
+    saveUninitialized: true
+}));
 
-    routes(app) //, db);
+app.use(passport.initialize());
+app.use(passport.session());
 
-    var port = 8080
-    app.listen(port, function () {
-        console.log('Listening on port '+port+'...');
-    });
+routes(app, passport);
 
-//});
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
+  console.log('Listening on port '+port+'...');
+});
+
