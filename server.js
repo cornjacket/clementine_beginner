c6923fa89afeb9c02afdc68e50ee60628f32a6d4
cornjacket <taylor.david.ray@gmail.com>
2015-11-18 18:52:20 +0000

    creating a mongoose model

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
 'use strict';
 
-var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongo = require('mongodb').MongoClient;
+var express = require('express'),
+    routes = require('./app/routes/index.js'),
+    mongoose = require('mongoose');
+
 
 var app = express();
 
-mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
+mongoose.connect('mongodb://localhost:27017/clementinejs');
 
-    if (err) {
-        throw new Error('Database failed to connect!');
-    } else {
-        console.log('MongoDB successfully connected on port 27017.');
-    }
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+app.use('/public', express.static(process.cwd() + '/public'));
 
-    app.use('/public', express.static(process.cwd() + '/public'));
-    app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+routes(app);
 
-    routes(app, db);
-
-    app.listen(8080, function () {
-        console.log('Listening on port 8080...');
-    });
-
+var port = 8080;
+app.listen(port, function () {
+    console.log('Node.js listening on port ' + port + '...');
 });
