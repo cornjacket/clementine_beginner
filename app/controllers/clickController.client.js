616ca860cbd5485842d94c06a48cae13addff682
cornjacket <taylor.david.ray@gmail.com>
2015-11-29 01:46:53 +0000

    add AngularJs to use with Click api

--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -1,62 +1,30 @@
-'use strict';
-
 (function () {
-   var addButton = document.querySelector('.btn-add');
-   var deleteButton = document.querySelector('.btn-delete');
-   var clickNbr = document.querySelector('#click-nbr');
-   var apiUrl = 'https://clementine-beginner-git-cornjacket.c9users.io/api/clicks';
-   
-   function ready (fn) {
-      if (typeof fn !== 'function') {
-         return;
-      }
 
-      if (document.readyState === 'complete') {
-         return fn();
-      }
+angular
+   .module('clementineApp', ['ngResource'])
+   .controller('clickController', ['$scope', '$resource', function ($scope, $resource) {
+      var Click = $resource('/api/clicks');
 
-      document.addEventListener('DOMContentLoaded', fn, false);
-   }   
-   
-   function ajaxRequest (method, url, callback) {
-      var xmlhttp = new XMLHttpRequest();
-
-      xmlhttp.onreadystatechange = function () {
-         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-            callback(xmlhttp.response);
-         }
+      $scope.getClicks = function () {
+        Click.get(function (results) {
+          $scope.clicks = results.clicks;
+        });
       };
 
-      xmlhttp.open(method, url, true);
-      xmlhttp.send();
-   }   
-   
-   function updateClickCount (data) {
-      var clicksObject = JSON.parse(data);
-      clickNbr.innerHTML = clicksObject.clicks;
-   }   
-   
-   // when DOM loads, go ahead and get click count from server
-   ready(ajaxRequest('GET', apiUrl, updateClickCount));
-   
-   // when user hits add button, post it to server, then call get,
-   // and when get completes, go ahad and update the click count
-   addButton.addEventListener('click', function () {
+      $scope.getClicks();
 
-      ajaxRequest('POST', apiUrl, function () {
-         ajaxRequest('GET', apiUrl, updateClickCount)
-      });
+      $scope.addClick = function () {
+         Click.save(function () {
+            $scope.getClicks();
+         });
+      };
+      
+      $scope.resetClicks = function () {
+         Click.remove(function () {
+            $scope.getClicks();
+         });
+      };
+      
+   }]);
 
-   }, false);   
-   
-   // when user hits reset button, send delete to server, ten call get,
-   // and when get completes, go ahead and update the click count
-   deleteButton.addEventListener('click', function () {
-
-      ajaxRequest('DELETE', apiUrl, function () {
-         ajaxRequest('GET', apiUrl, updateClickCount);
-      });
-
-   }, false);   
-   
 })();
