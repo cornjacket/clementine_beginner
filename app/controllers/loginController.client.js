c0200eec3d04339c967a79c95f919605b114cbca
cornjacket <taylor.david.ray@gmail.com>
2015-12-20 05:42:39 +0000

    loginController integrated

--- app/controllers/loginController.client.js
+++ app/controllers/loginController.client.js
@@ -2,27 +2,15 @@
 
 (function () {
 
-   angular
-      .module('pollPosition')
-      .controller('loginController', ['$scope', 'User',
-          function ($scope, User) {
-         
-        //$scope.pollHeader = "All Polls"
-        //$scope.displayAllPolls = true
-        //$scope.users           = {}
-        //$scope.num_polls       = 0 
-        //$scope.user.isLoggedIn = false // will this generate an error
-        //$scope.polls           = [] 
-        
-        $scope.user = {}
-        
+  angular
+    .module('pollPosition')
+    .controller('loginController', ['$scope', 'User', function ($scope, User) {
+      $scope.user = {}
         console.log("LOGIN CLIENT HAS STARTED")
-  
         User.get().then(function(user) {
           $scope.user = user
-        })
+      })
 
-
-  }]);
+    }]);
    
 })();
