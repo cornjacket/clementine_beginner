32304d0e8852e19634b4c813ba512e7273b73ccd
cornjacket <taylor.david.ray@gmail.com>
2016-01-01 19:04:03 +0000

    resolve now works in app.client

--- app/controllers/app.client.js
+++ app/controllers/app.client.js
@@ -10,36 +10,14 @@
              templateUrl: "public/main.html",
              controller: "mainController",
              resolve: {
-                 data1: function($q, $timeout, User, Poll) {
+                 data: function($q, $timeout, User, Poll) {
                      var defer = $q.defer()
-                      
-                      
-                      //User.all() //.then(Poll.getPolls({})).then(defer.resolve) //getPolls should have a user parameter - this may not work right
-                     
-//getUsers() // user_lookup/score_board
-
-  User.get().then(function(user) { // needed if index.html loginController is not on body
-   // $scope.user = user 
-    User.all()
-    console.log("DAVE TESTING POST USER.GET")
-    Poll.getPolls(user).then(function(polls){
-         // $scope.polls = polls
-          //$scope.num_polls = polls.length // this will need to be updated periodically
-          console.log("DAVE TESTING Defer.resolve HERE")
-          //console.log($scope.polls)
-          //$scope.currentpoll = $scope.polls[3] // this should be removed after i fix this bug
-          //console.log($scope.currentpoll)
-          defer.resolve()
-      })
-  })                     
-                     
-                     
-                     //$timeout(function() {
-                     //    defer.resolve()
-                     //    console.log("RESOLVE finished")
-                     //}, 10000)
-                     //
-                     console.log("DAVID: RETURN DEFERRED PROMISE")
+                      User.all()
+                      User.get().then(function(user) {
+                        Poll.getPolls(user).then(function(polls){
+                              defer.resolve()
+                          })
+                      })                     
                      return defer.promise
                  }
              }
